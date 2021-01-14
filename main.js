f331220023d622e64d42b1ed086a5773b06d1823
Varulvsnatt <jorge.gdp@gmail.com>
2021-01-13 22:35:50 -0300

    Weando

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ document.querySelector(".chooseScissors").addEventListener("click", () => choose
 function chooseFrom(str) {
     let playerChoice = document.querySelector(".playerChoice");
     playerChoice.setAttribute("style", `background-image: url(./img/${str}.jpg);`);
+    computerImg()
  }
 
  let rock = document.querySelector(".chooseRock").addEventListener("click", () => returnChoice("Rock"));
@@ -55,37 +56,37 @@ function chooseFrom(str) {
 //     }
 //     else if (playerSelection === "scissors" && computerSelection === "rock"){
 //     return "You lose! Rock beats Scissors!";   
+// //     }
+// //     else return "You draw";
+// // }
+
+// function game() {
+//         let playerCount = 0;
+//         let computerCount = 0;
+//         let playerScore = document.querySelector("#playerScore");
+//         let computerScore = document.querySelector("#computerScore");
+//         for (let i=0; i<5;i++){
+//             let resultHuman = sad;
+//             let resultComputer = computerImg();
+
+//             if (resultHuman === "Rock" && resultComputer == "Scissors"||resultHuman === "Paper" && resultComputer == "Rock"|| 
+//                         resultHuman === "Scissors" && resultComputer == "Paper") {
+//                 playerScore.textContent = playerCount++;
+//                 }
+//             else if (resultHuman === "Rock" && resultComputer == "Paper"||resultHuman === "Paper" && resultComputer == "Scissors"|| 
+//                         resultHuman === "Scissors" && resultComputer == "Rock") {
+//                 computerScore.textContent = computerCount++;
+//                 }
+        
 //     }
-//     else return "You draw";
+
+//     if (playerCount > computerCount) {
+//         alert(`Player won ${playerCount} to ${computerCount}`);
+//     }
+//     else if (playerCount < computerCount) {
+//         alert(`Player lost ${playerCount} to ${computerCount}`);
+//     }
+//     else alert("You draw!");
 // }
 
-function game() {
-        let playerCount = 0;
-        let computerCount = 0;
-        let playerScore = document.querySelector("#playerScore");
-        let computerScore = document.querySelector("#computerScore");
-        for (let i=0; i<5;i++){
-            let resultHuman = sad;
-            let resultComputer = computerImg();
-
-            if (resultHuman === "Rock" && resultComputer == "Scissors"||resultHuman === "Paper" && resultComputer == "Rock"|| 
-                        resultHuman === "Scissors" && resultComputer == "Paper") {
-                playerScore.textContent = playerCount++;
-                }
-            else if (resultHuman === "Rock" && resultComputer == "Paper"||resultHuman === "Paper" && resultComputer == "Scissors"|| 
-                        resultHuman === "Scissors" && resultComputer == "Rock") {
-                computerScore.textContent = computerCount++;
-                }
-        
-    }
-
-    if (playerCount > computerCount) {
-        alert(`Player won ${playerCount} to ${computerCount}`);
-    }
-    else if (playerCount < computerCount) {
-        alert(`Player lost ${playerCount} to ${computerCount}`);
-    }
-    else alert("You draw!");
-}
-
-//game();
+// //game();
